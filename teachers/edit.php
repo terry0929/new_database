126c775cc5640670e112276b3163839002ec2ebf
terry <D1285182@o365.fcu.edu.tw>
2025-06-14 04:35:40 +0800

    這次是真的最終版了

--- teachers/edit.php
+++ teachers/edit.php
@@ -27,7 +27,7 @@ $selected_categories = explode(',', $row['category'] ?? '');
 
 <div class="page-content">
     <h2>✏️ 編輯個人資料</h2>
-        <form action="update.php" method="post" enctype="multipart/form-data">
+        <form action="update.php" method="post" enctype="multipart/form-data" onsubmit="return validateForm()">
         <input type="hidden" name="teacher_id" value="<?= $row['teacher_id'] ?>">
 
         <label><h3>姓名</h3><br><input type="text" name="name" value="<?= $row['name'] ?>" style="width:80%; padding: 10px; font-size: 16px;"></label><br><br>
